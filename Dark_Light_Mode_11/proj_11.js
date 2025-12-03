ff3b9da82672e4c5b07aa55cefb3c15a0278de97
Puneet Kumar <puneetks2004@gmail.com>
2025-12-03 23:05:01 +0530

    updated

---
+++ Dark_Light_Mode_11/proj_11.js
@@ -0,0 +1,23 @@
+
+const toggleButton = document.getElementById("theme");
+const img = document.getElementById("ima");
+
+toggleButton.addEventListener("click", () => {
+    const html = document.querySelector("html");
+    if (html.classList.toggle("dark"))
+    {
+        img.src = "dark.jpg";
+    }
+    else {
+        img.src = "light.jpg";
+    }
+    localStorage.setItem("theme", html.classList.contains("dark") ? "dark" : "light");
+})
+
+// window.onload = () => {
+//     const theme = localStorage.getItem("theme");
+//     const html = document.querySelector("html");
+//     if (theme === "dark") {
+//         html.classList.add("dark");
+//     }
+// }
